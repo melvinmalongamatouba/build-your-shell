c286e798c897187bdfce5cb3f92d50c20e6b1c5c
melvinmalongamatouba <melvinmalongamatouba@gmail.com>
2025-10-29 21:36:19 +0100

    fixed read issue not taking the whole line

--- src/main.c
+++ src/main.c
@@ -1,39 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char* read(char* command);
-char* eval(char* command);
+char* read();
+int eval(const char* command, char* output);
 void print(char* output);
+bool hasPrefix(const char* command, const char* prefix);
+
+char empty_string[1024];
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
 
   while (true)
   {
-    char command[1024];
-    read(command);
-    char* output = eval(command);
+    char *command  = read();
+    char *output = calloc(1024, sizeof(char));
+    int res = eval(command, output);
+    if ((res == 0)||(res ==1))
+      exit(res);
     print(output);
   }
 
   return 0;
 }
 
-char* read(char* command)
+char* read()
 {
+  char *line = nullptr;
+  size_t lineSize = 0;
+  //printf("at read\n");
   printf("$ ");
-  scanf("%s", command ,stdin);
-  return command;
+  getline(&line, &lineSize , stdin);
+  if (lineSize > 1)
+    line[strlen(line) - 1] = '\0';
+  return line;
 
 }
 
-char* eval(char* command)
+bool hasPrefix(const char* command, const char* prefix)
 {
-  char* output = calloc(1024, sizeof(char));
+  if (strlen(prefix) > strlen(command))
+    return false;
+  return (strncmp(prefix, command, strlen(prefix)-1) == 0);
+}
+
+char suffix(const char* string)
+{
+  if (string == NULL || strlen(string) == 0)
+    return false;
+  return string[strlen(string) - 1]-48;
+}
+
+int eval(const char* command, char* output)
+{
+  //printf("at eval\n");
+  strcpy(output, empty_string);
+  //printf("%d" , hasPrefix(command, "exit"));
+  if (hasPrefix(command, "exit"))
+  {
+    return suffix(command);
+  }
   strcpy(output, command);
   const char* suffix = ": command not found\n";
   strcat(output, suffix);
-  return output;
+  return -1;
 }
 
 void print(char* output)
