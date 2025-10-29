af53440e3d5fb6f1f196bfdb803d8849f5ccb651
melvinmalongamatouba <melvinmalongamatouba@gmail.com>
2025-10-29 20:21:26 +0100

    REPL

--- src/main.c
+++ src/main.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+char* read(char* command);
+char* eval(char* command);
+void print(char* output);
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
-  printf("$ ");
-  char command[1024];
-  scanf("%s", command ,stdin);
-  printf("%s: command not found", command);
+
+  while (true)
+  {
+    char command[1024];
+    read(command);
+    char* output = eval(command);
+    print(output);
+    printf("%s: command not found", command);
+  }
 
   return 0;
+}
+
+char* read(char* command)
+{
+  printf("$ \n");
+  scanf("%s", command ,stdin);
+  return command;
+
+}
+
+char* eval(char* command)
+{
+  return ("%s : command not found\n");
+}
+
+void print(char* output)
+{
+  printf("%s", output);
 }
